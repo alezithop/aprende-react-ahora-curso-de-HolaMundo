3cac8946b03d5249c8272af021f55d02a895375a
= <=>
2025-12-26 15:57:31 -0600

    Pasando props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Card from "./components/Card";
 
 function App() {
   // código jsx aquí -> se va a transformar a llamadas de React.createElement
-  return <Card />;
+  return <Card body={"Hola Mundo"} />;
 }
 
 export default App;
