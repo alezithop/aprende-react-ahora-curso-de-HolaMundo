97a771b36356b57422dfca2c39846858ae29c67b
= <=>
2025-12-26 16:19:09 -0600

    Multiples props y props opcionales

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import Card from "./components/Card";
+import { CardBody } from "./components/Card";
 
 function App() {
   // código jsx aquí -> se va a transformar a llamadas de React.createElement
-  return <Card body={"Hola Mundo"} />;
+  return (
+    <CardBody
+      title={"Hola Mundo"}
+      text={"Este es un ejemplo de texto para la tarjeta."}
+      // text2={"Este es un texto opcional."} // <- Este texto es un parametro opcional
+    />
+  );
 }
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,14 +16,19 @@ function Card(props: CardProps) {
   );
 }
 
-export function CardBody() {
+interface CardBodyProps {
+  title: string;
+  text: string;
+  text2?: string;
+}
+
+export function CardBody(props: CardBodyProps) {
+  const { title, text, text2: opcionalText } = props;
   return (
     <>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card’s content.
-      </p>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
+      {opcionalText && <p className="card-text">{opcionalText}</p>}
       <a href="#" className="btn btn-primary">
         Go somewhere
       </a>
